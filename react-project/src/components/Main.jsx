777853805dba02e23138b5d8a0e0d16309e2ec60
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-08-12 12:19:08 +0200

    Header, Main , Footer, NavBar React

---
+++ react-project/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+    return (
+    <section>
+        <h2>This is my Main section</h2>
+    </section>
+    ) 
+}
---
+++ react-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#home">About</a></li>
+                <li><a href="#home">Contact</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}   
