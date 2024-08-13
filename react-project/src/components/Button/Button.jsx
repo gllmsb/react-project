7446ad0520eb0131f9d5c2f168a49c41f0bc13ca
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-08-13 13:25:44 +0200

    NOTESsss

--- react-project/src/components/Button/Button.jsx
+++ react-project/src/components/Button/Button.jsx
@@ -1,7 +1,9 @@
 import styles from './Button.module.scss';
 export const Button = ({action, size = 'medium', theme = 'light', text}) => {
+    // const { action, size = 'medium', theme = 'light', text } = props;
+    //  when using props as an object so basically u just using destructured props difference is that direct props is better for offering flexibility for more complex components or scenario while the destructured props preferred for simpler components where u only need specific props
     const buttonClass = `${styles.button} ${styles[size]} ${styles[theme]}`;
-    
+    // The ${styles[size]} syntax?? is basically used because it is dynamic, and has no specific styling until its declared in my understanding, since there are 3 sizes and 2 themes. otherwise it wouldve been fx. style.small, style.medium etc...  
     return (
         <button
             className = {buttonClass}
@@ -10,5 +12,5 @@ export const Button = ({action, size = 'medium', theme = 'light', text}) => {
             {text}
         </button>
     );
-};
+};  
 
